1ece636ffbd49968cfe98929554a41303d2b23f2
Bunyod0301 <bunyodyusupov0@gmail.com>
2024-05-02 14:54:47 +0500

    update

--- routes/home.js
+++ routes/home.js
@@ -3,8 +3,54 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
+    "data": {
+      "P2S": {
+        "months": [
+          "Nov"
+        ],
+        "sum": [
+          1100000
+        ],
+        "name": "P2S"
+      },
+      "ALL": {
+        "months": [
+          "Jun",
+          "Nov",
+          "Dec",
+          "Apr",
+          "May"
+        ],
+        "sum": [
+          1100000,
+          25300000,
+          555550,
+          1100000,
+          1100000
+        ],
+        "name": "ALL"
+      },
+      "P2P": {
+        "months": [
+          "Jun",
+          "Nov",
+          "Dec",
+          "Apr",
+          "May"
+        ],
+        "sum": [
+          1100000,
+          24200000,
+          555550,
+          1100000,
+          1100000
+        ],
+        "name": "P2P"
+      }
+    },
+    "errorKey": null,
+    "errorMessage": null,
+    "timestamp": 1714639594400
   });
 });
 
